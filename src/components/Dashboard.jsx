c605afe1d9a1e734ff16876e37659b7bc38eaa32
mrted <mailmrted@gmail.com.com>
2022-04-07 00:06:24 +0100

    Fleshed out dashboard component

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+      <div>
+          <div>
+            <div>logo</div>
+            <div>logo</div>
+            <div>logo</div>
+            <div>logo</div>
+            <div>logo</div>
+            <div>logo</div>
+            <div>logo</div>
+          </div>
+          <div>
+              <div>
+                  <div>Dashboard</div>
+                  <div>Search</div>
+                  <div>
+                      <div>notifications</div>
+                      <div>Profile Image</div>
+                      <div>Profile Name</div>
+                      <div>notifications</div>
+                      <div>Language</div>
+                  </div>
+              </div>
+              <div>
+                  <div>
+                      <div>Total Resident</div>
+                      <div>key icon</div>
+                  </div>
+                  <div>
+                      <div>Active Visitors</div>
+                      <div>Active visitors Icon</div>
+                  </div>
+                  <div>
+                      <div>Check In</div>
+                      <div>Check In Icon</div>
+                  </div>
+                  <div>
+                      <div>Check Out</div>
+                      <div>Check Out Icon</div>
+                  </div>
+              </div>
+              <div>
+                  <div>Total Payment</div>
+                  <div>Pie Chart</div>
+                  <div>Calendar</div>
+              </div>
+              <div>
+                <div>
+                    <h1>Avatar</h1>
+                    <h1>Category</h1>
+                    <h1>Date</h1>
+                    <h1>Priority</h1>
+                  </div>
+                  <div>
+                      <h1>Recent Payments</h1>
+                  </div>
+              </div>
+          </div>
+    </div>
+  )
+}
+
+export default Dashboard
